92e32e7068f7b92e9393954181f14583e5005010
editorbank <>
2020-04-29 00:29:51 +0300

    Добавил ещё чуть описания

--- all.cmd
+++ all.cmd
@@ -1,6 +1,7 @@
 @if not exist "%~dp0nginx.exe" (echo Not found file "%~dp0nginx.exe"! Copy it here. )&& goto :eof
 @if "%*"=="" (echo Use: call %~n0 ^<command^> [^<arg1^> [^<arg2^> [...]]] )&& goto :eof
 @for %%D in (
+
 app8031
 app8032
 
@@ -11,12 +12,13 @@ proxy8021
 proxy8022
 
 testin8000
+
 ) do @(
-  echo \\ %%D ...
   if exist %%D (
+    echo \\ %%D ...
     pushd %%D
     %*
     popd
+    echo // %%D .
   )
-  echo // %%D .
 )
